e06f9adb352cacd1a0a9c1f460d71359c35c4cd0
Harshad Parulekar <harshadhparulekar@gmail.com>
2017-05-22 19:41:32 +0530

    Update and fix installation

--- install.sh
+++ install.sh
@@ -8,11 +8,20 @@ echo "Symlinking dotfiles from ${BASEDIR}"
 
 # BASH
 ln -s ${BASEDIR}/vimrc ~/.vimrc
-ln -s ${BASEDIR}/bash_aliases
+ln -s ${BASEDIR}/bash_aliases ~/.bash_aliases
 ln -s ${BASEDIR}/bash_colors ~/.bash_colors
 ln -s ${BASEDIR}/bash_options ~/.bash_options
 ln -s ${BASEDIR}/bash_exports ~/.bash_exports
 ln -s ${BASEDIR}/bash_functions ~/.bash_functions
-ln -s ${BASEDIR}/extra ~/.extra
 ln -s ${BASEDIR}/bash_prompt ~/.bash_prompt
 ln -s ${BASEDIR}/bash_profile ~/.bash_profile
+
+
+# HOMEBREW
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# NVM
+\curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+
+# RVM
+\curl -sSL https://get.rvm.io | bash -s stable
